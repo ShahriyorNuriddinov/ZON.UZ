329b99ede13a64d13703c9076cd33bbafd1898e3
ShahriyorNuriddinov <nuriddinovlar20@gmail.com>
2025-11-02 11:48:52 +0500

    like and card page

--- main.js
+++ main.js
@@ -1,11 +1,6 @@
 const swiper = new Swiper(".mySwiper", {
   slidesPerView: 1,
-  spaceBetween: 30,
   loop: true,
-  autoplay: {
-    delay: 3000,
-    disableOnInteraction: false,
-  },
   pagination: {
     el: ".swiper-pagination",
     clickable: true,
